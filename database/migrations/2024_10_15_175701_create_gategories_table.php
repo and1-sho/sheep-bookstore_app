b633386f8356f891c767cef3425664bf4fc021e2
shooon <i.sho6125@icloud.com>
2024-10-15 21:05:22 +0900

    [Admin]マイグレーションファイルの追加

---
+++ database/migrations/2024_10_15_175701_create_gategories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gategories', function (Blueprint $table) {
+            $table->id('category_id');  // 主キー
+            $table->string('category_name');  // カテゴリー名
+            $table->timestamps();  // 作成日と更新日
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gategories');
+    }
+};
