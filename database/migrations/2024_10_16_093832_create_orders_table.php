e64bbecfe68e1cf4d00ab142a2561a694cf5977d
shooon <i.sho6125@icloud.com>
2024-10-16 09:45:44 +0900

    [Admin,User]3つのマイグレーションファイルを追加

---
+++ database/migrations/2024_10_16_093832_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();  // 主キー
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');  // 外部キー：usersテーブルに関連
+            $table->date('order_date');  // 注文日
+            $table->string('status');  // 注文の状態（例：processing, shipped, deliveredなど）
+            $table->decimal('total_amount', 10, 2);  // 注文の合計金額
+            $table->timestamps();  // 作成日と更新日
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2024_10_16_094053_create_carts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();  // 主キー
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');  // 外部キー：usersテーブルに関連
+            $table->timestamps();  // 作成日と更新日
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+};
