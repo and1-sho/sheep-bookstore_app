19d3d4573fc88b7628e3c356d82caa59dde9d557
shooon <i.sho6125@icloud.com>
2024-10-16 11:13:28 +0900

    [Admin,User]3つのマイグレーションファイルの追加と一つの変更

---
+++ database/migrations/2024_10_16_110022_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();  // 主キー
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');  // 外部キー：categoriesテーブルに関連
+            $table->string('product_name');  // 商品名
+            $table->text('description');  // 商品の説明
+            $table->decimal('price', 10, 2);  // 価格
+            $table->integer('stock');  // 在庫数
+            $table->string('image')->nullable();  // 商品画像
+            $table->timestamps();  // 作成日と更新日
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
