c4ca941307b745b096c88817f36e50fcc14b7755
ringo-117 <ykrina.117@gmail.com>
2024-10-17 17:28:19 +0900

    [Admin] ログアウト用のモーダルを追加

--- resources/js/components/AdminHeader.vue
+++ resources/js/components/AdminHeader.vue
@@ -1,13 +1,31 @@
 <template>
     <template class="header">
-        <!-- TOPページのリンク -->
+        <!-- ロゴのリンク -->
         <a href="#">
             <article class="logo">
                 <img class="logo_img" :src="logoImage" alt="Logo">
                 <h1>SHeep</h1>
             </article>
         </a>
-        <img class="icon_login" :src="loginIcon" alt="Login Icon">
+
+        <img class="icon_login open" :src="loginIcon" alt="Login Icon">
+
+        <!-- モーダル -->
+        <div class="modal_bg"></div>
+        <div class="container">
+            <div class="close">
+                <i></i>
+                <i></i>
+            </div>
+            <img class="icon_admin" :src="loginIcon" alt="Login Icon">
+
+            <!-- ログインしているユーザー名が入ります -->
+            <p class="user_name">ユーザー名</p>
+
+            <!-- ログアウトのリンク -->
+            <button class="Logout" onclick="location.href='#'">ログアウト</button>
+        </div>
+
     </template>
 </template>
 
@@ -44,5 +62,89 @@ const loginIcon = new URL('../../images/icon_login.png', import.meta.url).href;
 .icon_login {
     width: 22px;
     height: 22px;
+    cursor: pointer;
+}
+
+/* モーダル */
+.container {
+    width: 330px;
+    height: 280px;
+    position: fixed;
+    top: 60px;
+    right: 50px;
+    background-color: #ffffff;
+    opacity: 0;
+    pointer-events: none; /* モーダルを閉じたときに要素も消せる */
+    text-align: center;
+    border-radius: 10px;
+    align-content: center;
+    transition: 0.3s;
+}
+
+.container.active{
+  opacity: 1;
+  pointer-events: auto;
+}
+
+/* 閉じるボタン */
+.close {
+    cursor: pointer;
+}
+
+.close i {
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    height: 1px;
+    width: 20px;
+    background-color: #222222;
+    margin-top: 15px;
+}
+.close i:nth-of-type(1){
+    top: 5px;
+    transform: translateY(4px) rotate(45deg);
+}
+.close i:nth-of-type(2){
+    top: 13px;
+    transform: translateY(-4px) rotate(-45deg);
+}
+
+/* モーダルの中 */
+.icon_admin {
+    margin: 0 auto;
+    width: 68px;
+    margin-bottom: 14px;
+}
+
+.user_name {
+    font-size: 16px;
+    font-weight: bold;
+    color: #222222;
+}
+
+.Logout {
+    width: 190px;
+    height: 40px;
+    margin-top: 30px;
+    background-color: #524E4E;
+    border-radius: 3px;
+    color: #ffffff;
+}
+
+/* モーダル背景 */
+.modal_bg {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.6);
+    pointer-events: none;
+    opacity: 0;
+    transition: 0.3s;
+}
+
+.modal_bg.active {
+    opacity: 1;
 }
 </style>
