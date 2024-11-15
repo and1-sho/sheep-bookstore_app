846a5da4daf10fbe2520afe94cc6e26eb286a2ce
shooon <i.sho6125@icloud.com>
2024-11-15 15:43:04 +0900

    [User]詳細画面の追加

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -53,7 +53,8 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
+        $product = Product::findOrFail($id);
+        return view('show', compact('product'));
     }
 
     /**
